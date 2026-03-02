7893bca11e48d1746f918fd5fdde9531c3c180bb
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-02 22:34:19 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h1>Mock Test Platform</h1>
+      <p>Your site is working 🎉</p>
+    </div>
+  );
+}
