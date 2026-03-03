b5232d2184cdf8e95f0341653ddacf38730fcfcf
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-03 14:11:13 +0530

    Create page.js

---
+++ app/subjects/[id]/page.js
@@ -0,0 +1,18 @@
+export default function Exams({ params }) {
+
+  const exams = Array.from({ length: 10 }, (_, i) => i + 1);
+
+  return (
+    <div style={{textAlign:"center",marginTop:"100px"}}>
+      <h2>Exams for Subject {params.id}</h2>
+
+      {exams.map((exam) => (
+        <div key={exam} style={{margin:"10px"}}>
+          <a href={`/exam/${params.id}/${exam}`}>
+            Exam {exam}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
