004ed91a69e703652a24f934477391d4d8820c4f
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-03 23:49:58 +0530

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      localStorage.setItem("token", data.token);
+      alert("Login Successful");
+      window.location.href = "/subjects";
+    } else {
+      alert(data.message);
+    }
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Login</h2>
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
