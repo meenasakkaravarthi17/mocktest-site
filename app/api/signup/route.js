55b50a8fcdd20b97ac2e541cad571773e7aee0c4
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-03 22:43:12 +0530

    Create route.js

---
+++ app/api/signup/route.js
@@ -0,0 +1,44 @@
+import connectDB from "@/lib/mongodb";
+import User from "@/models/User";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const { name, email, password } = await req.json();
+
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "All fields required" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return NextResponse.json(
+      { message: "User created successfully" },
+      { status: 201 }
+    );
+
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Server error" },
+      { status: 500 }
+    );
+  }
+}
