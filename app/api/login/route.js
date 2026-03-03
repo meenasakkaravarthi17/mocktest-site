c5cd146ec6ba41cd5a98e9b77a661e0e7ccab125
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-03 23:47:16 +0530

    Create route.js

---
+++ app/api/login/route.js
@@ -0,0 +1,40 @@
+import { connectDB } from "../../../lib/mongodb";
+import User from "../../../models/User";
+import jwt from "jsonwebtoken";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const { email, password } = await req.json();
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return new Response(JSON.stringify({ message: "User not found" }), {
+        status: 400,
+      });
+    }
+
+    if (user.password !== password) {
+      return new Response(JSON.stringify({ message: "Invalid password" }), {
+        status: 400,
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    return new Response(
+      JSON.stringify({ message: "Login successful", token }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ message: "Server error" }), {
+      status: 500,
+    });
+  }
+}
