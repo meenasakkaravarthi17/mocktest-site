bf7a01de3fbb8bdac353dda9c4afd446a22da2c7
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-03 19:07:11 +0530

    Update page.js

--- app/payment/[subject]/[exam]/page.js
+++ app/payment/[subject]/[exam]/page.js
@@ -1,29 +1,65 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Payment({ params }) {
 
-  return (
-    <div style={{textAlign:"center",marginTop:"100px"}}>
-      <h2>Payment Page</h2>
+  const handlePayment = async () => {
 
-      <p>Subject: {params.subject}</p>
-      <p>Exam: {params.exam}</p>
+    const res = await fetch("/api/create-order", {
+      method: "POST",
+    });
+
+    const order = await res.json();
+
+    if (!order.id) {
+      alert("Order creation failed");
+      return;
+    }
+
+    const options = {
+      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: "INR",
+      name: "Mock Test",
+      description: "Exam Payment",
+      order_id: order.id,
+      handler: function () {
+        window.location.href = `/exam/${params.subject}/${params.exam}`;
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Secure Payment</h2>
 
       <button
-        onClick={() =>
-          window.location.href = `/exam/${params.subject}/${params.exam}`
-        }
+        onClick={handlePayment}
         style={{
-          padding:"10px 20px",
-          background:"green",
-          color:"white",
-          border:"none",
-          borderRadius:"5px",
-          marginTop:"20px",
-          cursor:"pointer"
+          padding: "10px 20px",
+          background: "green",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          marginTop: "20px",
+          cursor: "pointer",
         }}
       >
-        Pay ₹99 (Demo)
+        Pay ₹99
       </button>
     </div>
   );
