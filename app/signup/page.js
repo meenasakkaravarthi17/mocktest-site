9f181550b014174fc71f1030189cb86dae36cd04
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-03 22:55:02 +0530

    Create page.js

---
+++ app/signup/page.js
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    const data = await res.json();
+
+    alert(data.message);
+  };
+
+  return (
+    <div style={{
+      minHeight: "80vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <form
+        onSubmit={handleSignup}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "300px",
+          gap: "15px"
+        }}
+      >
+        <h2>Signup</h2>
+
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  );
+}
