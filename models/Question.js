567dd023d9cc4ca4aa144810c65ed625847c1356
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-08 09:06:08 +0530

    Update Question.js

--- models/Question.js
+++ models/Question.js
@@ -1,1 +1,43 @@
 
+import mongoose from "mongoose";
+
+const QuestionSchema = new mongoose.Schema(
+  {
+    subject: {
+      type: String,
+      required: true,
+    },
+    examNumber: {
+      type: Number,
+      required: true,
+    },
+    questionText: {
+      type: String,
+      required: true,
+    },
+    optionA: {
+      type: String,
+      required: true,
+    },
+    optionB: {
+      type: String,
+      required: true,
+    },
+    optionC: {
+      type: String,
+      required: true,
+    },
+    optionD: {
+      type: String,
+      required: true,
+    },
+    correctAnswer: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Question ||
+  mongoose.model("Question", QuestionSchema);
