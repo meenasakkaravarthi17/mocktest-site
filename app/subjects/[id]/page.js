29b93bb5a39cc22a142b8fbc1f1995c2f7e1c39d
meenasakkaravarthi17 <meenasakkaravarthi17@gmail.com>
2026-03-08 12:04:38 +0530

    Update page.js

--- app/subjects/[id]/page.js
+++ app/subjects/[id]/page.js
@@ -1,16 +1,13 @@
 export default function Exams({ params }) {
-
   const exams = Array.from({ length: 10 }, (_, i) => i + 1);
 
   return (
-    <div style={{textAlign:"center",marginTop:"100px"}}>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h2>Exams for Subject {params.id}</h2>
 
       {exams.map((exam) => (
-        <div key={exam} style={{margin:"10px"}}>
-          <a href={`/payment/${params.id}/${exam}`}>
-            Exam {exam}
-          </a>
+        <div key={exam} style={{ margin: "10px" }}>
+          <a href={`/payment/${params.id}/${exam}`}>Exam {exam}</a>
         </div>
       ))}
     </div>
